bec861739172b70d3ae9aa2dc8a8d66fdd42ef0d
Tanel Põld <tanel@brightside.ee>
2021-11-24 11:02:07 +0200

    [FEATURE] Show pagination options for link nr and url segment

--- Classes/Hooks/PageLayoutView/ContentElementPreviewRenderer.php
+++ Classes/Hooks/PageLayoutView/ContentElementPreviewRenderer.php
@@ -29,8 +29,10 @@ class ContentElementPreviewRenderer implements PageLayoutViewDrawItemHookInterfa
                 }
             }
 			$itemContent .= '<ul style="margin: 0; margin-top: 0.5em; padding: 0.2em 1.4em;">';
-    		$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Template: ' . $row['tx_gallerycontent_template']), $row) . '</li>';
-			if ($row['tx_gallerycontent_showtitle'] === 1) {
+            if ($row['tx_gallerycontent_template'] != 0) {
+                $itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Template: ' . $row['tx_gallerycontent_template']), $row) . '</li>';
+            }
+            if ($row['tx_gallerycontent_showtitle'] === 1) {
 				$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Show titles: yes'), $row) . '</li>';
 			}
 			if ($row['tx_gallerycontent_showdesc'] === 1) {
@@ -43,12 +45,18 @@ class ContentElementPreviewRenderer implements PageLayoutViewDrawItemHookInterfa
 			if ($row['image_zoom'] === 1) {
 				$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Click-enlarge crop: ' . $row['tx_gallerycontent_cropratiozoom']), $row) . '</li>';
 			}
-			if ($row['tx_paginatedprocessors_paginationenabled'] === 1) {
-				$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Pagination: enabled'), $row) . '</li>';
-			}
-			if ($row['tx_paginatedprocessors_itemsperpage'] && $row['tx_paginatedprocessors_paginationenabled'] === 1) {
-				$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Items per page: ' . $row['tx_paginatedprocessors_itemsperpage']), $row) . '</li>';
-			}
+            if ($row['tx_paginatedprocessors_paginationenabled'] == 1) {
+                $itemContent .= '<li>' . $parentObject->linkEditContent('Pagination: enabled', $row) . '</li>';
+            }
+            if ($row['tx_paginatedprocessors_itemsperpage'] && $row['tx_paginatedprocessors_paginationenabled'] == 1) {
+                $itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Items per page: ' . $row['tx_paginatedprocessors_itemsperpage']), $row) . '</li>';
+            }
+            if ($row['tx_paginatedprocessors_pagelinksshown'] && $row['tx_paginatedprocessors_paginationenabled'] == 1) {
+                $itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Links shown: ' . $row['tx_paginatedprocessors_pagelinksshown']), $row) . '</li>';
+            }
+            if ($row['tx_paginatedprocessors_urlsegment'] && $row['tx_paginatedprocessors_paginationenabled'] == 1) {
+                $itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('URL segment: ' . $row['tx_paginatedprocessors_urlsegment']), $row) . '</li>';
+            }
 			$itemContent .= '</ul>';
 			$drawItem = FALSE;
 		}
