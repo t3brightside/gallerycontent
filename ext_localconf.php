06bf1a8a1f029c8e298765b945760e213d88f227
Tanel Põld <tanel@brightside.ee>
2021-12-19 12:31:22 +0200

    [TASK] Put icons to right folders

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@
 	$iconRegistry->registerIcon(
 		'mimetypes-x-content-gallerycontent',
 		\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-		['source' => 'EXT:gallerycontent/Resources/Public/Images/Icons/mimetypes-x-content-gallerycontent.svg']
+		['source' => 'EXT:gallerycontent/Resources/Public/Icons/mimetypes-x-content-gallerycontent.svg']
 	);
 
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['gallerycontent'] = \Brightside\Gallerycontent\Hooks\PageLayoutView\ContentElementPreviewRenderer::class;
