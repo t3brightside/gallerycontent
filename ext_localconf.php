668eb745937525c0647462ec74813b9c4a26c3d8
Tanel Põld <tanel@brightside.ee>
2023-02-19 13:00:46 +0200

    [TASK] Follow coding guidelines for ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,32 +1,34 @@
 <?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
-$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
-// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
-if ($versionInformation->getMajorVersion() < 12) {
-   ExtensionManagementUtility::addPageTSConfig('
-      @import "EXT:gallerycontent/Configuration/page.tsconfig"
-   ');
-}
+defined('TYPO3') || die('Access denied.');
 
-$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-$iconRegistry->registerIcon(
-	'mimetypes-x-content-gallerycontent',
-	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-	['source' => 'EXT:gallerycontent/Resources/Public/Icons/mimetypes-x-content-gallerycontent.svg']
-);
+(function () {
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+    // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+    if ($versionInformation->getMajorVersion() < 12) {
+        ExtensionManagementUtility::addPageTSConfig('
+            @import "EXT:gallerycontent/Configuration/page.tsconfig"
+        ');
+    }
 
+    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'mimetypes-x-content-gallerycontent',
+        SvgIconProvider::class,
+        ['source' => 'EXT:gallerycontent/Resources/Public/Icons/mimetypes-x-content-gallerycontent.svg']
+    );
 
-ExtensionUtility::configurePlugin(
-	'gallerycontent',
-	'Gallerycontent',
-	[
-		'Gallerycontent' => 'gallerycontent'
-	]
-);
+    ExtensionUtility::configurePlugin(
+        'gallerycontent',
+        'Gallerycontent',
+        [
+            'Gallerycontent' => 'gallerycontent'
+        ]
+    );
+})();
