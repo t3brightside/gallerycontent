fd980f8056ad814bd694af7c514d858505b2b127
Tanel Põld <tanel@brightside.ee>
2023-02-19 13:01:12 +0200

    [TASK] Code cleanup

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,7 +1,5 @@
 <?php
 
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'gallerycontent',
     'Configuration/TypoScript',
