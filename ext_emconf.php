820022fcd99254b23d1b1bdc671102b2535ec311
Tanel Põld <tanel@brightside.ee>
2023-03-20 16:06:05 +0200

    [TASK] Release 1.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Gallerycontent',
     'description' => 'Content element for image gallery with preset crop ratios and pagination',
     'category' => 'fe',
-    'version' => '1.5.3',
+    'version' => '1.6.0',
     'state' => 'stable',
     'clearcacheonload' => true,
     'author' => 'Tanel Põld',
@@ -15,6 +15,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '11.5.0 - 12.99.99',
             'fluid_styled_content' => '11.5.0 - 12.99.99',
             'paginatedprocessors' => '1.4.2 - 1.99.99',
+            'embedassets' => '1.2.0-1.99.99',
         ],
     ],
     'autoload' => [
